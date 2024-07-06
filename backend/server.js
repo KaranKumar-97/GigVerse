46f448fc375e3c624560c03db7a845c19416332d
Karan Kumar <karankumar.msit@gmail.com>
2024-07-06 23:59:00 +0530

    deployment testing

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ const app=express()
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb'}));
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:5173",credentials:true}));
+// app.use(cors({origin:"http://localhost:5173",credentials:true}));
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 const connect = async () =>{
     try {
