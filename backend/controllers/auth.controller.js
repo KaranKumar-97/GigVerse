b290d0e56f4282527297605ee1a0d16caf715828
Karan Kumar <karankumar.msit@gmail.com>
2024-07-13 00:12:23 +0530

    login persist fix

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -43,6 +43,7 @@ const login = async (req, res) => {
       httpOnly: true, 
       secure: true, 
       maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: 'none',
     }).json(user)
 
   } catch (error) {
